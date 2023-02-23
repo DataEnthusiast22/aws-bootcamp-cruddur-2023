fcfd62a570cabe6a1bb43e16fbaa3e447d8b7a3d
DataEnthusiast22 <ccgserv@protonmail.com>
2023-02-23 13:53:35 +0000

    dockerfile CMD as an external script

---
+++ backend-flask/ext_flask.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# CMD = command. it is used to run flask app
+# this basically says run flask module using python version 3 listening on host 0.0.0.0 port 4567
+python3 -m flask run --host=0.0.0.0 --port=4567
